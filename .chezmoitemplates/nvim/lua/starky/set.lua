a929ba7e23ca10f5d3cb1c3836765c4c67872c0a
Starky the fox <14891772+starkythefox@users.noreply.github.com>
2023-12-09 22:41:06 +0100

    Set listchars

--- .chezmoitemplates/nvim/lua/starky/set.lua
+++ .chezmoitemplates/nvim/lua/starky/set.lua
@@ -7,10 +7,16 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
+vim.opt.listchars:append {
+    tab = '> ',
+    trail = '-',
+    nbsp = '+',
+    eol = '$',
+}
 
 vim.opt.swapfile = false
 vim.opt.backup = false
