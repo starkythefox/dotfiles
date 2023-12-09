6d9083bc567c168d68bf61dbd1ae23fc3c1c76fc
Starky the fox <14891772+starkythefox@users.noreply.github.com>
2023-12-09 22:40:16 +0100

    Add Open with Trouble for Telescope

--- .chezmoitemplates/nvim/after/plugin/telescope.lua
+++ .chezmoitemplates/nvim/after/plugin/telescope.lua
@@ -1,7 +1,14 @@
 local telescope = require('telescope')
 local telescope_builtin = require('telescope.builtin')
+local trouble = require('trouble.providers.telescope')
 
 telescope.setup({
+    defaults = {
+        mappings = {
+            i = { ["<c-t>"] = trouble.open_with_trouble },
+            n = { ["<c-t>"] = trouble.open_with_trouble },
+        },
+    },
     pickers = {
         find_files = {
             hidden = true,
--- .chezmoitemplates/nvim/after/plugin/which-key.lua
+++ .chezmoitemplates/nvim/after/plugin/which-key.lua
@@ -5,6 +5,7 @@ wk.register({
     ['<C-p>'] = {'(LSP) Select previons item'},
     ['<C-y>'] = {'(LSP) Confirm item'},
     ['<C-Space>'] = {'(LSP) Complete'},
+    ['<C-t>'] = {'Open with trouble'},
     ["]f"] = {"Next function start"},
     ["[f"] = {"Previous function start"},
     ["]c"] = {"Next class start"},
@@ -14,3 +15,7 @@ wk.register({
     ["[C"] = {"Previous class end"},
     ["]C"] = {"Next class end"},
 }, {mode = 'n'})
+
+wk.register({
+    ['<C-t>'] = {'Open with trouble'},
+}, {mode = 'i'})
