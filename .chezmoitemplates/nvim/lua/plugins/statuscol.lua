251e25b9f2e87165805e2c645773a66d244c8bd9
starkythefox <14891772+starkythefox@users.noreply.github.com>
2024-03-01 01:27:01 +0100

    Add missing trailing commas on statuscol

--- .chezmoitemplates/nvim/lua/plugins/statuscol.lua
+++ .chezmoitemplates/nvim/lua/plugins/statuscol.lua
@@ -7,17 +7,17 @@ return {
                 segments = {
                     {
                         sign = {name = {".*"}, maxwidth = 1, colwidth = 1, auto = true, wrap = true},
-                        click = "v:lua.ScSa"
+                        click = "v:lua.ScSa",
                     },
                     {
                         sign = {name = {"Diagnostic"}, maxwidth = 1, auto = true},
-                        click = "v:lua.ScSa"
+                        click = "v:lua.ScSa",
                     },
                     {
                         text = {builtin.lnumfunc}, click = "v:lua.ScLa",
                     },
                     {
-                        text = {builtin.foldfunc}, click = "v:lua.ScFa"
+                        text = {builtin.foldfunc}, click = "v:lua.ScFa",
                     },
                     {
                         sign = {namespace = {"gitsign*"}, maxwidth = 1, colwidth = 1, auto = false},
